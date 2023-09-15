ef864767562abf435ecac23c5aed1c45055b119c
pravallikasuram <141315840+pravallikasuram@users.noreply.github.com>
2023-09-15 19:25:10 +0530

    commit

--- logout.php
+++ logout.php
@@ -6,21 +6,7 @@ session_start();
 
 include('db.php');
 
-// if (isset($_SESSION["username"])) {
-//     $username = $_SESSION["username"];
-//     $sql = "SELECT username FROM users WHERE username = '$username'";
-//     $result = $conn->query($sql);
 
-//     if ($result->num_rows == 1) {
-//         $row = $result->fetch_assoc();
-//         $username = $row["username"];
-//         $last_login = $row["last_login"];
-
-//         // Update previous_logout timestamp
-//         $update_previous_logout_sql = "UPDATE users SET previous_logout = '$last_login' WHERE username = '$username'";
-//         $conn->query($update_previous_logout_sql);
-//     }
-// }
 
 session_destroy();
 header("Location: login.php");
